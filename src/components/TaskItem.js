08dc7f024125b80c90cd0a99d4d219bd6ea258e3
Fluffy-Lion <leoncooper31@gmail.com>
2021-08-11 21:38:18 +0100

    added polyline transition

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,18 +1,52 @@
 import styled from "styled-components";
 import React from "react";
+import { enumDeclaration } from "@babel/types";
 const TaskCont = styled.div`
   border: solid grey 4px;
   // display: flex;
   // align-items: center;
   // justify-content: center;
+  background: #5ca4ea;
+  color: #fff;
 `;
 
 const Button = styled.button`
-  border: red solid 4px;
+//   border: red solid 4px;
   height: 30px;
   margin: 5px;
 
   position: relative;
+`;
+const PolyOne = styled.polyline`
+  fill: #5ca4ea;
+  stroke: #91c9ff;
+  transition: all 0.8s ease-in-out;
+
+  ${Button}:hover & {
+    fill: #4f95da;
+  }
+`;
+const PolyTwo = styled.polyline`
+  stroke: #fff;
+  stroke-dasharray: 40 480;
+  stroke-dashoffset: 40;
+  transition: all 0.8s ease-in-out;
+
+  ${Button}:hover & {
+    stroke-dashoffset: -480;
+  }
+`;
+
+const Span = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+  line-height: 58px;
+  font-size: 18px;
+  letter-spacing: 1px;
+  font-weight: 300;
 `;
 
 const TaskItem = ({ task, deleteHandler, index }) => {
@@ -20,14 +54,13 @@ const TaskItem = ({ task, deleteHandler, index }) => {
     <TaskCont>
       <h2>your task is....{task}</h2>
       <Button onClick={() => deleteHandler(index)}>
-        <span>delete</span>
+        <Span>delete</Span>
+
+        <svg width="180px" height="60px" viewBox="0 0 80 20">
+          <PolyOne points="179,1 179,59 1,59 1,1 179,1" />
+          <PolyTwo points="179,1 179,59 1,59 1,1 179,1" />
+        </svg>
       </Button>
-      <svg height="200" width="500">
-        <polyline
-          points="20,20 40,25 60,40 80,120 120,140 200,180"
-          style={{ fill: "none", stroke: "black", strokeWidth: 3 }}
-        />
-      </svg>
     </TaskCont>
   );
 };
