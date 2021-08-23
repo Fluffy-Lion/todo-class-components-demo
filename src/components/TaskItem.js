7438f403b997107fd7521de19023dbd5954d503e
Fluffy-Lion <leoncooper31@gmail.com>
2021-08-23 20:01:00 +0100

    added svg animation to delete

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import React from "react";
-import { enumDeclaration } from "@babel/types";
+
 const TaskCont = styled.div`
   border: solid grey 4px;
   // display: flex;
@@ -10,23 +10,22 @@ const TaskCont = styled.div`
   color: #fff;
 `;
 
-const Button = styled.button`
-//   border: red solid 4px;
-  height: 30px;
+const Button = styled.div`
+  // border: red solid 4px;
+  height: 29px;
+  width: 99px;
   margin: 5px;
-
   position: relative;
 `;
 const PolyOne = styled.polyline`
-  fill: #5ca4ea;
+
   stroke: #91c9ff;
   transition: all 0.8s ease-in-out;
 
-  ${Button}:hover & {
-    fill: #4f95da;
-  }
 `;
 const PolyTwo = styled.polyline`
+  // stroke: #000;
+
   stroke: #fff;
   stroke-dasharray: 40 480;
   stroke-dashoffset: 40;
@@ -34,20 +33,31 @@ const PolyTwo = styled.polyline`
 
   ${Button}:hover & {
     stroke-dashoffset: -480;
+    fill: black;
   }
 `;
 
 const Span = styled.span`
+
   position: absolute;
-  top: 0;
+  height: 90%;
+  top: 4px;
   left: 0;
   right: 0;
   text-align: center;
-  line-height: 58px;
+  // line-height: 58px;
   font-size: 18px;
   letter-spacing: 1px;
   font-weight: 300;
+  
 `;
+
+const Svg = styled.svg `
+  fill: #5C5DEA;
+  width: 99px;
+  height: 30px;
+  viewBox: 0 0 80 20;
+`
 
 const TaskItem = ({ task, deleteHandler, index }) => {
   return (
@@ -56,10 +66,12 @@ const TaskItem = ({ task, deleteHandler, index }) => {
       <Button onClick={() => deleteHandler(index)}>
         <Span>delete</Span>
 
-        <svg width="180px" height="60px" viewBox="0 0 80 20">
-          <PolyOne points="179,1 179,59 1,59 1,1 179,1" />
-          <PolyTwo points="179,1 179,59 1,59 1,1 179,1" />
-        </svg>
+        <Svg 
+        // width="180px" height="60px" viewBox="0 0 80 20"
+        >
+          <PolyOne points="98,1 98,28 1,28 1,1 98,1" />
+          <PolyTwo points="98,1 98,28 1,28 1,1 98,1" />
+        </Svg>
       </Button>
     </TaskCont>
   );
